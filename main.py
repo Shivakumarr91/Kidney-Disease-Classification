e87055e83a1249eb36e3e663bd0b416f30217429
Shivakumar R <shivakumarr914837@gmail.com>
2024-10-14 09:12:17 +0530

    Model Training Added

--- main.py
+++ main.py
@@ -27,3 +27,14 @@ except Exception as e:
         raise e  
 
     
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
